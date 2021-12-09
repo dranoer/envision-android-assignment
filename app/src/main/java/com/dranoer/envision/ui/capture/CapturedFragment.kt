07eb2d71258400bdfcb0436927e76542022889d3
dranoer <dranoer@gmail.com>
2021-12-09 18:31:22 +0330

    add: Snackbar for navigation to library screen

--- app/src/main/java/com/dranoer/envision/ui/capture/CapturedFragment.kt
+++ app/src/main/java/com/dranoer/envision/ui/capture/CapturedFragment.kt
@@ -1,15 +1,19 @@
 package com.dranoer.envision.ui.capture
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.dranoer.envision.Constants
+import com.dranoer.envision.R
 import com.dranoer.envision.databinding.FragmentCapturedBinding
 import com.dranoer.envision.ui.listener.NavigationListener
 import com.dranoer.envision.ui.listener.TabListener
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -44,11 +48,35 @@ class CapturedFragment : Fragment() {
 
         binding.saveButton.setOnClickListener {
             params?.let { it1 -> viewModel.saveParagraph(it1) }
+
+            with(binding.saveButton) {
+                isEnabled = true
+                setBackgroundColor(Color.GRAY)
+            }
+            openSnack(view)
         }
 
         return view
     }
 
+    fun openSnack(view: View) {
+        val snackbar =
+            Snackbar.make(view, getString(R.string.text_saved), Snackbar.LENGTH_INDEFINITE)
+
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(Color.WHITE)
+        val textView =
+            snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
+        textView.setTextColor(Color.DKGRAY)
+        textView.textSize = 14f
+
+        snackbar.setActionTextColor(Color.MAGENTA)
+        snackbar.setAction(resources.getString(R.string.go_to_library)) {
+            navigationListener!!.openLibrary()
+        }
+
+        snackbar.show()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
