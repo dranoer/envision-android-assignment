12637ba153b20ddc89e350a869a0cb314640f26a
dranoer <dranoer@gmail.com>
2022-01-08 21:24:10 +0330

    add: Extension for snackbar

---
+++ app/src/main/java/com/dranoer/envision/util/ViewExt.kt
@@ -0,0 +1,13 @@
+package com.dranoer.envision.util
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * Transforms static java function Snackbar.make() to an extension function on View.
+ */
+fun View.showSnackbar(snackbarText: String, timeLength: Int) {
+    Snackbar.make(this, snackbarText, timeLength).run {
+        show()
+    }
+}
